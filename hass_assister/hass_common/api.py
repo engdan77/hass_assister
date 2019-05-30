4a3bbb82832d6cb7f465b6e0343aec040437abd6
Daniel Engvall <daniel@engvalls.eu>
2019-05-30 08:34:27 +0200

    Adding capability to get info from hass entity

--- hass_assister/hass_common/api.py
+++ hass_assister/hass_common/api.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict
 import aiohttp
 from hass_assister.scheduler import MyScheduler
 from loguru import logger
@@ -7,12 +7,13 @@ import fs
 import yaml
 
 class HassInstance(object):
-    def __init__(self, url: str, auth_token: str, scheduler: Union[MyScheduler, None] = None, update_freq: int = 10):
+    def __init__(self, url: str, auth_token: str, scheduler: Union[MyScheduler, None] = None, host: str, share: str = 'homeassistant', update_freq: int = 10):
         self.url = url
         self.auth_token = auth_token
         self.scheduler = scheduler
         self.update_freq = update_freq
         self.attributes = None
+        self.config = self.update_configuration(host, share)
 
         logger.debug('initializing HassInstance object')
         if scheduler:
@@ -26,7 +27,17 @@ class HassInstance(object):
             entity_count = len(nested_lookup('entity_id', self.attributes))
             logger.debug(f'{entity_count} entities pulled')
 
-    def read_configuration(self, host, share):
+    def update_configuration(self, host, share):
         f = fs.open_fs(f'smb://{host}/{share}').open('configuration.yaml').replace('!', '')
-        y = yaml.load(f)
+        self.config = yaml.load(f)
+
+    def get_entity_info(self, mqtt_topic: str) -> Dict:
+        c = self.config
+        config_lists = [c[root_key] for root_key in c.keys() if type(c[root_key]) is list]
+        valid_entities = [_dict
+                            for _list in config_lists
+                            for _dict in _list
+                            if 'platform' in _dict and _dict['platform'] == 'mqtt']
+        return next([e for e in valid_entities if e['state_topic'] == mqtt_topic], None)
+
 
