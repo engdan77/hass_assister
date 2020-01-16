c1648e6dd2d182801b5040e4e4d11a50f9c7e31a
Daniel Engvall <daniel@engvalls.eu>
2020-01-16 21:28:16 +0100

    Fixing import_item to support packages

--- hass_assister/helper/api.py
+++ hass_assister/helper/api.py
@@ -3,11 +3,11 @@ import importlib
 
 def import_item(item):
     if type(item) is str:
-        module, attr = item.rsplit('.') if '.' in item else (None, item)
+        *module, attr = item.split('.')
         if not module:
             f = globals()[attr]
         else:
-            f = getattr(importlib.import_module(module), attr)
+            f = getattr(importlib.import_module('.'.join(module)), attr)
     else:
         f = item
     return f
