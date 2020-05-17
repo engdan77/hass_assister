79109878af393024bb7792ba50a1f25d13507141
Daniel Engvall <daniel@engvalls.eu>
2020-05-17 21:41:47 +0200

    Fixing so settings only states creating folder when really is doing that

--- hass_assister/settings/api.py
+++ hass_assister/settings/api.py
@@ -29,8 +29,9 @@ def init_settings(_default_config_params):
             f"no local config directory, creating settings in {base_config_dir}"
         )
     conf_path = Path(base_config_dir) / Path(f"{p}.yaml")
-    conf_path.parent.mkdir(parents=True, exist_ok=True)
-    logger.info(f"creating {conf_path}")
+    if not conf_path.exists():
+        conf_path.parent.mkdir(parents=True, exist_ok=True)
+        logger.info(f"creating {conf_path}")
     conf_obj = easyconf.Config(str(conf_path))
     conf = {}
     for param, initials in _default_config_params.items():
